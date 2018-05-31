75c3445bf1f938ab58cd078446c8be3c1eec0839
Woodslake <woodslakelee@gmail.com>
2018-05-31 13:37:34 +0800

    ...

--- springboot-mybatis-generator/src/main/java/com/woods/mybatis/SpringbootMybatisGeneratorApplication.java
+++ springboot-mybatis-generator/src/main/java/com/woods/mybatis/SpringbootMybatisGeneratorApplication.java
@@ -1,11 +1,9 @@
 package com.woods.mybatis;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.woods.mybatis.dao")
 public class SpringbootMybatisGeneratorApplication {
 
     public static void main(String[] args) {
--- springboot-mybatis-generator/src/main/java/com/woods/mybatis/dao/UserMapper.java
+++ springboot-mybatis-generator/src/main/java/com/woods/mybatis/dao/UserMapper.java
@@ -1,16 +1,12 @@
 package com.woods.mybatis.dao;
 
 import com.woods.mybatis.domain.User;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.InsertProvider;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
-import org.apache.ibatis.annotations.UpdateProvider;
+import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.type.JdbcType;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@Mapper
 public interface UserMapper {
     @Delete({
         "delete from user",
